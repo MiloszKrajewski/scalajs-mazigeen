4126a23b8ad0e0138280712d385bb387cb687855
Milosz Krajewski <miloszkrajewski@o2.pl>
2016-09-27 00:38:26 +0100

    tweaks

--- src/main/scala/mazigeen/Model.scala
+++ src/main/scala/mazigeen/Model.scala
@@ -19,16 +19,12 @@ class Exit(val A: Room, val B: Room, val weight: Double) extends Edge[Room]
 class Model(size: Point, random: () => Double) {
     private val rooms = Array.tabulate(size.x, size.y) { (x, y) => new Room(Point(x, y)) }
 
-    private def getAt(x: Int, y: Int): Room = rooms(x)(y)
-    private def tryAt(x: Int, y: Int): Option[Room] =
-        if (x < 0 || y < 0 || x >= size.x || y >= size.y) None else Some(getAt(x, y))
-
-    private def connect(room: Room, other: Option[Room], weight: Double) =
-        other.foreach { room.add(_, weight) }
+    private def at(x: Int, y: Int): Room = rooms(x)(y)
+    private def connect(room: Room, other: Room, weight: Double) = room.add(other, weight)
 
     for (x <- 0 until size.x; y <- 0 until size.y) {
-        connect(getAt(x, y), tryAt(x - 1, y), random())
-        connect(getAt(x, y), tryAt(x, y - 1), random())
+        if (x > 0) connect(at(x, y), at(x - 1, y), random())
+        if (y > 0) connect(at(x, y), at(x, y - 1), random())
     }
 
     def room00 = rooms(0)(0)
