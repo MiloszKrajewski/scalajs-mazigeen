55016479ff72c9713f5b1d4dd4b9e9a2a7810ba1
Milosz Krajewski <miloszkrajewski@o2.pl>
2016-09-28 08:28:52 +0100

    presentation

--- src/main/scala/mazigeen/App.scala
+++ src/main/scala/mazigeen/App.scala
@@ -10,7 +10,11 @@ import scala.util.Random
 object UI {
     val WORLD_SIZE = Point(100, 100)
     val ROOM_SIZE = 6
-    val DOOR_SIZE = 1
+    val DOOR_SIZE = 2
+
+    val ROOM_COLOR = "#fff"
+    val DOOR_COLOR = "#eee"
+
     var handle: Option[Int] = None
 
     def toPixel(v: Int): Int = v * ROOM_SIZE + (v + 1) * DOOR_SIZE
@@ -29,24 +33,36 @@ object UI {
         val algorithm = new Prim[Room, Exit](model.room00)
 
         context.clearRect(0, 0, width, height)
-        context.lineWidth = ROOM_SIZE / 2
-        context.strokeStyle = "#fff"
+        context.fillStyle = ROOM_COLOR
+        context.strokeStyle = DOOR_COLOR
+        context.lineWidth = ROOM_SIZE * 0.9
 
         handle = Some(window.setInterval(() => step(algorithm, context), 0))
+    }
+
+    def drawRoom(context: CanvasRenderingContext2D, room: Room) = {
+        val p = toPixel(room.position)
+        context.fillRect(p.x, p.y, ROOM_SIZE, ROOM_SIZE)
+    }
+
+    def drawExit(context: CanvasRenderingContext2D, exit: Exit) = {
+        val a = toPixel(exit.A.position)
+        val b = toPixel(exit.B.position)
+        val o = ROOM_SIZE / 2
+        context.beginPath()
+        context.moveTo(a.x + o, a.y + o)
+        context.lineTo(b.x + o, b.y + o)
+        context.closePath()
+        context.stroke()
     }
 
     def step(algorithm: Prim[Room, Exit], context: CanvasRenderingContext2D) = {
         algorithm.next() match {
             case None => shutdown()
-            case Some(exit) =>
-                val a = toPixel(exit.A.position)
-                val b = toPixel(exit.B.position)
-                val o = ROOM_SIZE / 2
-                context.beginPath()
-                context.moveTo(a.x + o, a.y + o)
-                context.lineTo(b.x + o, b.y + o)
-                context.closePath()
-                context.stroke()
+            case Some(exit) => 
+                drawExit(context, exit)
+                drawRoom(context, exit.A)
+                drawRoom(context, exit.B)
         }
     }
 
